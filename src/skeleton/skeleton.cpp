0486d4787c1a3cbca6fed585f86239501b8cd183
Sylvain Joubert <joubert.sy@gmail.com>
2011-03-15 14:00:29 +0100

    Webcam inputs supported

--- src/skeleton/skeleton.cpp
+++ src/skeleton/skeleton.cpp
@@ -7,11 +7,33 @@
 
 using namespace std;
 
+enum InputType
+{
+    IMAGE,
+    VIDEO,
+    CAM,
+    NONE
+};
+
 void process(IplImage *colorSrcImg);
 
 int main(int argc, char** argv)
 {
-    if (argc == 3)
+    InputType input = NONE;
+    if (argc == 3 && strcmp(argv[1], "-i") == 0)
+    {
+        input = IMAGE;
+    }
+    else if (argc == 3 && strcmp(argv[1], "-v") == 0)
+    {
+        input = VIDEO;
+    }
+    else if (argc == 2 && strcmp(argv[1], "-w") == 0)
+    {
+        input = CAM;
+    }
+
+    if (input != NONE)
     {
         cvNamedWindow("Source", CV_WINDOW_AUTOSIZE);
         cvNamedWindow("Binary", CV_WINDOW_AUTOSIZE);
@@ -20,15 +42,14 @@ int main(int argc, char** argv)
         
         IplImage *colorSrcImg = NULL;
         CvCapture *capture = NULL;
-        if (strcmp(argv[1], "-i") == 0 &&
-            (colorSrcImg = cvLoadImage(argv[2])) != NULL)
+        
+        if (input == IMAGE && ((colorSrcImg = cvLoadImage(argv[2])) != NULL))
         {
             process(colorSrcImg);   
             cvReleaseImage(&colorSrcImg);
             cvWaitKey();
         }
-        else if(strcmp(argv[1], "-v") == 0 &&
-                (capture = cvCaptureFromFile(argv[2])) != NULL)
+        else if(input == VIDEO && (capture = cvCaptureFromFile(argv[2])) != NULL)
         {
             IplImage *tmp = NULL;
             while ((tmp = cvQueryFrame(capture)) != NULL)
@@ -37,6 +58,22 @@ int main(int argc, char** argv)
                 process(colorSrcImg);
                 cvReleaseImage(&colorSrcImg);
                 cvWaitKey(2);
+            }
+            cvReleaseCapture(&capture);
+            cvWaitKey();
+        }
+        else if (input == CAM && ((capture = cvCaptureFromCAM(-1)) != NULL))
+        {
+            IplImage *tmp = NULL;
+            while (true)
+            {
+                if ((tmp = cvQueryFrame(capture)) != NULL)
+                {
+                    colorSrcImg = cvCloneImage(tmp);
+                    process(colorSrcImg);
+                    cvReleaseImage(&colorSrcImg);
+                    cvWaitKey(2);
+                }
             }
             cvReleaseCapture(&capture);
             cvWaitKey();
@@ -60,7 +97,7 @@ void process(IplImage *colorSrcImg)
     /***** Threshold *****/
     cvThreshold(graySrcImg, graySrcImg, 240, 255, CV_THRESH_BINARY);
     cvMorphologyEx(graySrcImg, graySrcImg, NULL,
-                   cvCreateStructuringElementEx(3, 3, 1, 1, CV_SHAPE_RECT),
+                   cvCreateStructuringElementEx(5, 5, 1, 1, CV_SHAPE_RECT),
                    CV_MOP_CLOSE);
     cvShowImage("Binary", graySrcImg);
 
