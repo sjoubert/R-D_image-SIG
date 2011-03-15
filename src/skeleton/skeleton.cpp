fcc95cd78d3dd717b40e4fd6e5594f8bee8d6345
Sylvain Joubert <joubert.sy@gmail.com>
2011-03-15 12:55:26 +0100

    Video inputs supported

--- src/skeleton/skeleton.cpp
+++ src/skeleton/skeleton.cpp
@@ -7,51 +7,75 @@
 
 using namespace std;
 
+void process(IplImage *colorSrcImg);
+
 int main(int argc, char** argv)
 {
-    IplImage *colorSrcImg = NULL;
-
-int64 start = cvGetTickCount();
-    if (argc == 2 && (colorSrcImg = cvLoadImage(argv[1])) != NULL)
+    if (argc == 3)
     {
-        IplImage *graySrcImg = cvCreateImage(cvGetSize(colorSrcImg), IPL_DEPTH_8U, 1);
         cvNamedWindow("Source", CV_WINDOW_AUTOSIZE);
         cvNamedWindow("Binary", CV_WINDOW_AUTOSIZE);
         cvNamedWindow("Skeleton", CV_WINDOW_AUTOSIZE);
         cvNamedWindow("Intersections", CV_WINDOW_AUTOSIZE);
-
-        /***** Source *****/
-        cvShowImage("Source", colorSrcImg);
-        cvConvertImage(colorSrcImg, graySrcImg);
-
-        /***** Threshold *****/
-        cvThreshold(graySrcImg, graySrcImg, 240, 255, CV_THRESH_BINARY);
-        cvMorphologyEx(graySrcImg, graySrcImg, NULL,
-                       cvCreateStructuringElementEx(3, 3, 1, 1, CV_SHAPE_RECT),
-                       CV_MOP_CLOSE);
-        cvShowImage("Binary", graySrcImg);
-
-        /***** Skeleton *****/
-        Skeletonize skel(graySrcImg);
-        skel();
-        cvShowImage("Skeleton", graySrcImg);
-
-        /***** Intersections *****/
-        vector<CvPoint> crossroads = Intersections(graySrcImg)();
-cout << (cvGetTickCount() - start) / (1000.f * cvGetTickFrequency()) << " ms" << endl;
-        for (unsigned int i = 0; i < crossroads.size(); ++i)
+        
+        IplImage *colorSrcImg = NULL;
+        CvCapture *capture = NULL;
+        if (strcmp(argv[1], "-i") == 0 &&
+            (colorSrcImg = cvLoadImage(argv[2])) != NULL)
         {
-            cvCircle(colorSrcImg, crossroads[i], 2, CV_RGB(255, 0, 0), -1);
+            process(colorSrcImg);   
+            cvReleaseImage(&colorSrcImg);
+            cvWaitKey();
         }
-        cvShowImage("Intersections", colorSrcImg);
-
-        cvWaitKey();
-
-        cvReleaseImage(&graySrcImg);
-        cvReleaseImage(&colorSrcImg);
+        else if(strcmp(argv[1], "-v") == 0 &&
+                (capture = cvCaptureFromFile(argv[2])) != NULL)
+        {
+            IplImage *tmp = NULL;
+            while ((tmp = cvQueryFrame(capture)) != NULL)
+            {
+                colorSrcImg = cvCloneImage(tmp);
+                process(colorSrcImg);
+                cvReleaseImage(&colorSrcImg);
+                cvWaitKey(2);
+            }
+            cvReleaseCapture(&capture);
+            cvWaitKey();
+        }
+    
         cvDestroyWindow("Source");
         cvDestroyWindow("Binary");
         cvDestroyWindow("Skeleton");
         cvDestroyWindow("Intersections");
     }
+}
+
+void process(IplImage *colorSrcImg)
+{
+    IplImage *graySrcImg = cvCreateImage(cvGetSize(colorSrcImg), IPL_DEPTH_8U, 1);
+    
+    /***** Source *****/
+    cvShowImage("Source", colorSrcImg);
+    cvConvertImage(colorSrcImg, graySrcImg);
+
+    /***** Threshold *****/
+    cvThreshold(graySrcImg, graySrcImg, 240, 255, CV_THRESH_BINARY);
+    cvMorphologyEx(graySrcImg, graySrcImg, NULL,
+                   cvCreateStructuringElementEx(3, 3, 1, 1, CV_SHAPE_RECT),
+                   CV_MOP_CLOSE);
+    cvShowImage("Binary", graySrcImg);
+
+    /***** Skeleton *****/
+    Skeletonize skel(graySrcImg);
+    skel();
+    cvShowImage("Skeleton", graySrcImg);
+
+    /***** Intersections *****/
+    vector<CvPoint> crossroads = Intersections(graySrcImg)();
+    for (unsigned int i = 0; i < crossroads.size(); ++i)
+    {
+        cvCircle(colorSrcImg, crossroads[i], 2, CV_RGB(255, 0, 0), -1);
+    }
+    cvShowImage("Intersections", colorSrcImg);
+
+    cvReleaseImage(&graySrcImg);
 }
